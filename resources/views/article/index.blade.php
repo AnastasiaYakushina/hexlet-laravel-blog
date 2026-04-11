b6ce43e4423b1818cc276ed5fcc1f963e71cd96a
AnastasiaYakushina <aubarr@ya.ru>
2026-04-11 21:31:53 +0300

    Add create article

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
     <h1>Список статей</h1>
+    <a href="{{ route('articles.create') }}">Добавить новую статью</a>
     @foreach ($articles as $article)
-        <h2><a href="{{route('articles.show', $article->id)}}">{{$article->name}}</a></h2>
+        <h2><a href="{{ route('articles.show', $article->id) }}">{{ $article->name }}</a></h2>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
-        <div>{{Str::limit($article->body, 200)}}</div>
+        <div>{{ Str::limit($article->body, 200) }}</div>
     @endforeach
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,34 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hexlet Blog - @yield('title')</title>
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="csrf-param" content="_token" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
-    </head>
-    <body>
-        <div class="container mt-4">
-            <h1>@yield('header')</h1>
-            <div>
-                @yield('content')
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hexlet Blog - @yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-param" content="_token" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+
+<body>
+    <div class="container mt-4">
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
             </div>
+        @endif
+
+        @if (session('error'))
+            <div class="alert alert-danger">
+                {{ session('error') }}
+            </div>
+        @endif
+        <h1>@yield('header')</h1>
+        <div>
+            @yield('content')
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
